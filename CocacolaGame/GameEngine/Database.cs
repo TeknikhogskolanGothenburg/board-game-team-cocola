bdc275be131f64ba6a11dfeb00b0c4f7764be5c8
wilari932 <33053518+wilari932@users.noreply.github.com>
2018-01-23 16:49:34 +0100

    Blev Tvugen och lägga om Projektet fans massa buggar i Visual Studio

--- CocacolaGame/GameEngine/Database.cs
+++ CocacolaGame/GameEngine/Database.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data.Sql;
+using System;
 
 namespace GameEngine
 {
@@ -14,8 +12,9 @@ namespace GameEngine
         private SqlConnection Connection;
         public Database()
         {
+            // Wilmar DatabaseNyckel Data Source=LAPTOP-6J4IQ728\SQLEXPRESS;Initial Catalog=GameDB;Integrated Security=True
             // lenas databasnyckel Data Source=LAPTOP-AMB9IU8B\SQLEXPRESS;Initial Catalog=GameDB;Integrated Security=True
-            Connection = new SqlConnection(@"Data Source=LAPTOP-AMB9IU8B\SQLEXPRESS;Initial Catalog=GameDB;Integrated Security=True");
+            Connection = new SqlConnection(@"Data Source=LAPTOP-6J4IQ728\SQLEXPRESS;Initial Catalog=GameDB;Integrated Security=True;");
         }
         public string CreateKey()
         {
---
+++ CocacolaGame/fias/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GameEngine;
+
+namespace fias.Controllers
+{
+    public class HomeController : Controller
+    {
+        // skapar en lista av Spel
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GameChecker(string name = null)
+        {
+            if (name == null)
+            {
+                return RedirectToActionPermanent("CreateGame");
+            }
+            else
+            {
+                Database Game = new Database();
+                Game.InsertKeyInDataBase();
+
+                return RedirectToAction("GameStarter", "Home", new { Gamekey = Game.generatedKey });
+            }
+        }
+
+        public ActionResult CreateGame()
+        {
+
+            return View();
+        }
+
+        public ActionResult GameStarter(string Gamekey)
+        {
+            ViewBag.Key = Gamekey;
+            return View();
+        }
+
+     
+    }
+
+}
